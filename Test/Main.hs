1ac035b0cbbe99efa9fc4db3617393b973a52754
Froxwin <56168224+Froxwin@users.noreply.github.com>
2024-02-08 16:24:56 +0530

    unit testing with tasty

--- Test/Main.hs
+++ Test/Main.hs
@@ -1,13 +1,19 @@
-import Test.HUnit
-import System.Exit
+import Test.Tasty
+import Test.Tasty.HUnit
 
-test1 :: Test
-test1 = TestCase (assertEqual "should return 3" 3 (1 + 2))
+import Data.List
+import Data.Ord
 
-tests :: Test
-tests = TestList [TestLabel "test1" test1]
+main = defaultMain tests
 
-main :: IO ()
-main = do
-    result <- runTestTT tests
-    if failures result > 0 then exitFailure else exitSuccess
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests = testGroup "Unit tests"
+  [ testCase "List comparison (different length)" $
+      [1, 2, 3] `compare` [1,2] @?= GT
+
+  -- the following test does not hold
+  , testCase "List comparison (same length)" $
+      [1, 2, 3] `compare` [1,2,2] @?= GT
+  ]
