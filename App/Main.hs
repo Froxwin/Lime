4cd9ca38729df4bf9e84032716e3993c3855a8ee
Froxwin <56168224+Froxwin@users.noreply.github.com>
2024-02-08 17:37:37 +0530

    fixed tests

---
+++ App/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Engine as Lib(main)
+
+main :: IO ()
+main = Lib.main
--- Test/Main.hs
+++ Test/Main.hs
@@ -1,19 +1,20 @@
-import Test.Tasty
-import Test.Tasty.HUnit
+import           Camera
+import           Objects.Sphere
+import           Ray
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Vector
 
-import Data.List
-import Data.Ord
-
+main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
-unitTests = testGroup "Unit tests"
-  [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
-
-  -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= GT
+unitTests :: TestTree
+unitTests = testGroup
+  "Unit tests"
+  [ testCase "Hitting Sphere"
+    $   rayColor (Ray (Vector 0 0 0) (Vector 0 0 1)) [sphere (Vector 0 0 50) 10]
+    @?= Color 0.5 0.5 0
   ]
